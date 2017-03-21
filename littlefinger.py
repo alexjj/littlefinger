a2e4677bd9f3bb40289f201372e90fed905722a1
Alex Johnstone <alexjj@gmail.com>
2017-03-21 14:02:02 -0700

    Additional functions from learning
    https://nbviewer.jupyter.org/github/donnemartin/data-science-ipython-notebooks/blob/master/pandas/03.09-Pivot-Tables.ipynb

--- littlefinger.py
+++ littlefinger.py
@@ -1,21 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Littlefinger
+
+Performing calculations on personal financial transactions
+"""
+
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-# read Excel
-excel = pd.ExcelFile('Money1.2.xlsx')
 
-uk = excel.parse('UK')
-us = excel.parse('US')
+# Useful timeranges
+now = datetime.now()
+weekago = now - timedelta(7)
 
 def tidyxacts(df):
+    """
+    Fix formats of columns for transactions in dataframe df
+
+    Parameters
+    ----------
+    df : Dataframe
+
+    Returns
+    -------
+
+      Date formatted as Date
+      Amounts as numeric
+      Top and sub level categories as category
+    """
+
     df['Date'] = pd.to_datetime(df['Date'])
     df['Inflow'] = pd.to_numeric(df['Inflow'])
     df['Outflow'] = pd.to_numeric(df['Outflow'])
     df['Net'] = pd.to_numeric(df['Net'])
     df['Master Category'] = df['Master Category'].astype("category")
     df['Sub Category'] = df['Sub Category'].astype("category")
+
 
 def add_date_info(df):
     """
@@ -24,48 +48,76 @@ def add_date_info(df):
     df['Year'], df['Month'] = df['Date'].dt.year, df['Date'].dt.month
     df['Quarter'], df['Week'] = df['Date'].dt.quarter, df['Date'].dt.week
 
-def money_usd(df):
+
+def top10expenses(df, starttime, endtime):
     """
-    Format like money
-    """
-    df['Net'] = pd.to_string(df['Net'])
-    df['Net'] = df['Net'].apply('${:,.2f}'.format)
-    return df
+    Returns top 10 expenses between dates. 
     
-def money_gbp(df):
-    """
-    Format like money
-    """
-    return "£{:,.2f}".format(x)
+    Makes a smaller list of relevant columns and filters df.
     
+    TODO
+    ----
+    
+      Add categorical listing to expense/income to avoid showing income
+    
+    """
+    cols = ['Account', 'Date', 'Payee', 'Master Category', 'Sub Category',
+            'Outflow']
+    df2 = df[cols]
+    filtered = df2[(df2['Date'] >= starttime) & (df2['Date'] <= endtime)]
+    return filtered.sort_values(by='Outflow', ascending=False).head(10)
+
+
+def dateindex(df):
+    """
+    Returns df with index as date column
+    """
+    return df.set_index(['Date'])
+
+
+def account_balances(df):
+    """
+    Returns current account balances
+    """
+
+    return pd.pivot_table(df, index=["Account"], values=["Net"],
+                          aggfunc=np.sum).sort_values(by='Net')
+
+
+def annualspends(df):
+    """
+    Returns pivot table by year and master category.
+    """
+    return pd.pivot_table(uk, index=['Year', 'Master Category'],
+                          values=["Net"], aggfunc=np.sum)
+
+
+"""
+def money(x):
+    return "${:,.0f}".format(x)
+
+formatted_df = df_sub.applymap(money)
+formatted_df
+"""
+
+
+# Each account's balance
+def accounttrend(df):
+    """
+    Plot running total of each account over all time
+    """
+    grouped = df['Net'].groupby(df['Account'])
+    for account, net in grouped:
+        print(account)
+        net.cumsum().plot()
+
+
+# Open Excel and parse worksheets
+
+excel = pd.ExcelFile("~\python\Money1.2.xlsx")
+uk = excel.parse('UK')
+us = excel.parse('US')
 tidyxacts(uk)
 add_date_info(uk)
 tidyxacts(us)
 add_date_info(us)
-
-
-# Create Year/Month columns
-uk['Year'], uk['Month'] = uk['Date'].dt.year, uk['Date'].dt.month
-uk = uk.sort_values(by='Date')
-uk.head()
-
-"""
-Weekly
-
-    Top 10 expenses that week
-    Balance of main accounts
-    Week on week change
-    Date of last update (or most recent transaction)
-
-"""
-
-# Account balance
-ukacc = pd.pivot_table(uk,index=["Account"], values=["Net"],aggfunc=np.sum)
-usacc = pd.pivot_table(us,index=["Account"], values=["Net"],aggfunc=np.sum)
-# Combined in one table
-result = pd.concat([ukacc, usacc])
-print(result.sort_values(by='Net'))
-
-# use cumsum to get running total
-uk.Net.cumsum()
-
